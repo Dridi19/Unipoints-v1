8cb2e163a1e2030ddfc394ffe28cc6522d0872a5
Dridi19 <dridimohamed19@gmail.com>
2022-07-09 00:44:03 +0200

    Unipoints v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Login from "./login.js"
+import Rewards from "./Rewards.js"
+import Rewardpage from "./Rewardpage.js"
+import Scan from "./Scan.js"
+import Register from "./register.js"
+import Reduction from "./Reduction.js"
+import Maps from "./Maps.js"
+import Coupon from "./Coupon.js"
+
+import Settings from "./settings.js"
+import Mainpage from "./Mainpage.js"
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />}>
+          </Route>
+          <Route path="/login" element={<Login />}>
+          </Route>
+          <Route path="/maps" element={<Maps />}>
+          </Route>
+          <Route path="/" element={<Mainpage />}>
+          </Route>
+          <Route path="/home" element={<Rewards />}>
+          </Route>
+          <Route path="/scan" element={<Scan />}>
+          </Route>
+          <Route path="/rewards" element={<Rewardpage />}>
+          </Route>
+          <Route path="/reduction" element={<Reduction />}>
+          </Route>
+          <Route path="/settings" element={<Settings />}>
+          </Route>
+          <Route path="/coupon" element={<Coupon />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+</div>
+
   );
 }
 
